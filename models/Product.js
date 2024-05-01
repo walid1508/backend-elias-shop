65b1a7c53edef8c656d4ceeb7c4aa6332af4b2f9
walid1508 <2703593@collegelacite.ca>
2024-05-01 19:08:26 -0400

    Project scaffold

---
+++ models/Product.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        unique: true,
+        trim: true,
+        maxlength: [50, 'Name can not be more than 50 characters']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Please add a price'],
+        maxlength: [5, 'Price can not be more than 5 characters']
+    },
+    description: {
+        type: String,
+        required: [true, 'Please add a description'],
+        maxlength: [500, 'Description can not be more than 500 characters']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+productSchema.plugin(AutoIncrement, {inc_field: 'product_id'});
+
+module.exports = mongoose.model('Product', productSchema);
